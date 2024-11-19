77a3f13aee2af8e7000256991565b8ed4261b51e
hyuzenn <huj22641@naver.com>
2024-11-19 18:06:04 +0900

    교수님 코드 받아적기

--- net_lab/echoclient.c
+++ net_lab/echoclient.c
@@ -8,8 +8,39 @@
 
 int main(int argc, char** argv) 
 {
-	return 0;
+	int clientfd, prot;
+	struct socket_in serveraddr;
+	
+	char *host, bud[MAXLINE];
 
+	host = argv[1];
+	port = atoi(argv[2]);
+
+	if((clientfd = socket(AF_INET, SCOK_STREAM, 0)) < 0 ){
+		perror("socket");
+		exit(1);
+	}
+
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_port = htons((unsigned short)port);
+	serveraddr.sin_addr.s_addr = inet_addr(host);
+
+	if(connect(clientfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr)) < 0 ){
+		perror("connect");
+		exit(1);
+	}
+	
+	int rbytes, wbytes;
+	while(fgets(buf, MAXLINE, stdin) != NULL){
+		wbytes = write(clientfd, buf, MAXLINE);
+		if (wbytes < strlen(bur))
+			printf("Failed to send message. \n");
+		rbytes = read(clientfd, buf, MAXLINE);
+		fgets(buf, stdout);
+	}
+
+	close(clientfd);
+	return 0;
 }
 
 
--- net_lab/echoserver.c
+++ net_lab/echoserver.c
@@ -11,5 +11,49 @@
 
 int main(int argc, char **argv) 
 {
+	int listenfd, connfd, port, clientlen;
+  struct sockaddr_in serveraddr, clientaddr;
+	char buf[MAXLINE];
+
+	//Create a socket
+	if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ){
+		perror("socket");
+		exit(1);
+	}
+	
+	//Associate the socket with a pair of address and port number
+	port = atoi(argv[1]);
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serveraddr.sin_port = htons((unsigned short)port);
+
+	if (bind(listenfd, (struct socketaddr*)&serveraddr, sizeof(serveraddr)) == -1 ){
+		perror("Failed ro bind. ");
+		return -1;
+	}
+
+	//Start waiting
+	if (listen(listenfd, LISTENQ) < 0)
+		return -1;
+	
+	//Wait for connetion request
+	printf("Waiting for request... \n");
+
+	clientlen = sizeof(clientaddr);
+	if ((connfd = accept(listenfd, (struct sockaddr*)&clientaddr, &clientlen)) < 0 ){
+		perror("accept");
+		exit(-1);
+	}
+
+	char* caddrp = inet_ntoa(clientaddr.sin_addr);
+	printf("Connected to %s. \n", caddrp);
+
+	int n;
+	while (1){
+		n = read(connfd, buf, MAXLINE);
+		if (n == 0 ) break;
+		printf("Server received %d bytes. \n", n);
+		write(connfd, buf, MAXLINE);
+	}
 	return 0;
 }

346ba971b22518135f553d17471a75e6939db631
hyuzenn <huj22641@naver.com>
2024-12-02 15:28:44 +0900

    Signal Handling 1번 코드

---
+++ signal_lab/handler.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#define NUM 4
+
+void sig_int_handler(int sig){
+  fprintf(stdout, "Process %d received signal %d\n", getpid(), sig);
+  exit(0);
+}
+
+int main(int argc, void* argv[]){
+  pid_t pid[NUM];
+  int i, child_status;
+  signal(SIGINT, sig_int_handler);
+
+  for(i=0; i < NUM; i++){
+    if((pid[i] = fork()) == 0)
+      while(1);
+  }
+
+  for(i=0; i < NUM; i++) {    
+    printf("Killing processes\n");
+    kill(pid[i], SIGINT);
+  }
+
+  for(i=0; i < NUM; i++){
+    pid_t wpid = wait(&child_status);
+    if(WIFEXITED(child_status))
+      printf("Child %d terminated with exit status %di\n", wpid, WEXITSTATUS(child_status));
+    else
+      printf("Child %d terminated abnormally\n", wpid);
+  }
+  return 0;
+}
